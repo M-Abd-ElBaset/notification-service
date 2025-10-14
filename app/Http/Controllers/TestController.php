972642a2b62ae87618212c22a3d77ff4f0d69fa3
melgebaly <m.abdelbaset@silkysys.com>
2025-10-14 14:01:49 +0300

    added payOnline function to Test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Adapter\PaymentServiceInterface;
 use App\Services\NotificationService;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function __construct(private NotificationService $notificationService)
+    public function __construct(private NotificationService $notificationService,
+                                private PaymentServiceInterface $paymentService)
     {
         
     }
@@ -30,6 +32,28 @@ class TestController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'Failed to send notification: ' . $e->getMessage()
+            ], 400);
+        }
+
+    }
+
+    public function payOnline(Request $request)
+    {
+        $data = $request->validate([
+            'amount' => 'required|numeric|min:0.01'
+        ]);
+
+        try {
+            $result = $this->paymentService->processPayment($data['amount']);
+            
+            return response()->json([
+                'success' => $result,
+                'message' => 'Payment processed successfully.'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to process payment: ' . $e->getMessage()
             ], 400);
         }
 
