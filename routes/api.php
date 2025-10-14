20e03858836fd39c203fab50086881ae0121cb1a
melgebaly <m.abdelbaset@silkysys.com>
2025-10-14 14:01:32 +0300

    added payment route

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/notifications/send', [TestController::class, 'sendNotification']);
+Route::post('/payment/pay', [TestController::class, 'payOnline']);
